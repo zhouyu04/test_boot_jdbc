839b12fb26c43eddb9b292081a0daef758f01c89
zzyy <zzyy0320@163.com>
2018-11-04 11:27:32 +0800

    配置druid、log4j2

--- src/main/java/com/zzyy/controller/ItemController.java
+++ src/main/java/com/zzyy/controller/ItemController.java
@@ -2,7 +2,10 @@ package com.zzyy.controller;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.zzyy.DruidConfiguration;
 import com.zzyy.service.ItemService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/item")
 public class ItemController {
+
+    private Logger logger = LogManager.getLogger(DruidConfiguration.class);
 
     @Autowired
     ItemService itemService;
@@ -28,7 +33,7 @@ public class ItemController {
 
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public JSONObject list() {
-
+        logger.info("查询list");
         return itemService.list();
     }
 }
